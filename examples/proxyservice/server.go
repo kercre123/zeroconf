f6551a9034151ef6e0b87958ba4bdaa53d6a602d
kercre123 <kerigancreighton@gmail.com>
2023-12-20 20:22:23 -0600

    kercre rather than grandcat

--- examples/proxyservice/server.go
+++ examples/proxyservice/server.go
@@ -9,7 +9,7 @@ import (
 
 	"time"
 
-	"github.com/grandcat/zeroconf"
+	"github.com/kercre123/zeroconf"
 )
 
 var (
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/grandcat/zeroconf
+module github.com/kercre123/zeroconf
 
 go 1.13
 
